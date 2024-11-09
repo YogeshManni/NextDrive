7144252ce44bf1f5fce62ed8abce5d84d17af9a7
Yogesh Manni <yogeshmanni786@gmail.com>
2024-11-08 19:31:56 -0800

    init nextjs

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">Nextdrive</h1>
+    </div>
+  );
+}
